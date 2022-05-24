b9fb3c51de0f81f4d0d3346ada53089de1fb7173
Andressa Silva <andressaszx@gmail.com>
2022-05-24 10:05:55 -0300

    project created

---
+++ src/main/java/br/com/alura/aluraforum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.aluraforum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	@ResponseBody // retorno do metodo é direto no navegador
+	public String Hello() {
+		return "Hello World!";
+	}
+
+}
